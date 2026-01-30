4475ef5c8b6285df388d715569a4916243b15dea
SultanKorkmazov <ssultankorkmazov@gmail.com>
2026-01-30 21:27:45 +0500

    Add Practice2 - Python control flow examples (boolean, if-else, loops)

---
+++ practice_2/boolean/boolean_comprasion.py
@@ -0,0 +1,17 @@
+#Comparison operators return True or False based on the comparison:
+x = 5
+y = 3
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+#Chaining Comparison Operators
+x = 5
+
+print(1 < x < 10)
+
+print(1 < x and x < 10)
---
+++ practice_2/loops/while_loop.py
@@ -0,0 +1,13 @@
+#Print i as long as i is less than 6:
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+
+#Print a message once the condition is false:
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
