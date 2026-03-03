bf9cddab6e7a9cfe93a07ae750809e7263ae83a1
SultanKorkmazov <ssultankorkmazov@gmail.com>
2026-03-03 15:58:56 +0500

    Renamed python files

--- practice_5/tasks.py
+++ practice_5/tasks.py
@@ -1,49 +1,57 @@
-import re 
-with open("raw.txt", "r", encoding="utf-8") as f:
-    raw = f.read()
+import re
 
+with open("exmpl.txt", "r", encoding="utf-8") as f:
+    exmp = f.read()
 
-pattern = r"x\s(\d+.*)"
-prices = re.findall(pattern, raw)
-print("="*5, "Task 1", "="*5)
-print("All prices: ", *prices)
+# Task 1
+print("-"*10, "Task 1", "-"*10)
+pattern = r"\bab*\b"
+print(bool(re.search(pattern, exmp)))
 
-pattern = r"\d+\.\s(.+)"
-products = re.findall(pattern, raw)
-print('='*5, "Task 2", '='*5)
-print("All products: ")
-print(*products, sep='\n')
+# Task 2
+print("-"*10, "Task 2", "-"*10)
+pattern = r"\bab{2,3}\b"
+print(bool(re.search(pattern, exmp)))
 
-pattern = r"Стоимость\n(\d+.*)"
-total = re.findall(pattern, raw)
-res = 0
-print('='*5, "Task 3", '='*5)
-for i in total:
-    i = i.replace(',', '.')
-    i = i.replace(' ', '')
-    res += float(i)
-print("Total: ", res)
+# Task 3
+print("-"*10, "Task 3", "-"*10)
+pattern = r"\b[a-zа-я]+(?:_[a-zа-я]+)+\b"
+print(*re.findall(pattern, exmp), sep="\n")
 
-pattern = r"(\d{2}\.\d{2}\.\d{4}\s\d{2}:\d{2}:\d{2})"
-date = re.findall(pattern, raw)
-print("="*5, "Task 4", '='*5)
-print(*date)
+# Task 4
+print("-"*10, "Task 4", "-"*10)
+pattern = r"\b[A-ZА-Я][a-zа-я]+\b"
+print(*re.findall(pattern, exmp), sep="\n")
 
-pattern = r"(Банковская карта|Наличные|Kaspi|Visa|MasterCard)"
-payment = re.findall(pattern, raw)
-print('='*5, "Task 5", '='*5 )
-print(*payment)
+# Task 5
+print("-"*10, "Task 5", "-"*10)
+pattern = r"^a.*b$"
+print(*re.findall(pattern, exmp, re.MULTILINE), sep="\n")
 
-#Task 6
-import json 
+# Task 6
+print("-"*10, "Task 6", "-"*10)
+result = re.sub(r"[ ,\.]", ":", exmp)
+print(result)
 
-data = dict()
-data["prices"] = prices 
-data["products"] = products
-data["total"] = res 
-data["date"] = date 
-data["payment"] = payment 
-s = json.dumps(data, separators= (":" , ","))
+# Task 7
+print("-"*10, "Task 7", "-"*10)
+def to_camel(s):
+    return re.sub(r"_([a-z])", lambda m: m.group(1).upper(), s)
 
-with open("data.json", 'w', encoding="utf-8") as f:
-    json.dump(data, f, ensure_ascii=False)
+print(to_camel(exmp))
+
+# Task 8
+print("-"*10, "Task 8", "-"*10)
+pattern = r"(?=[A-ZА-Я])"
+result = re.split(pattern, exmp)
+print(*result, sep="\n")
+
+# Task 9
+print("-"*10, "Task 9", "-"*10)
+result = re.sub(r"(?<!^)([A-ZА-Я])", r" \1", exmp)
+print(result)
+
+# Task 10
+print("-"*10, "Task 10", "-"*10)
+result = re.sub(r"(?<!^)([A-ZА-Я])", r"_\1", exmp).lower()
+print(result)
