d93348fca509a7ce71f407f8f389d9bf0f6e96da
SultanKorkmazov <ssultankorkmazov@gmail.com>
2026-03-14 19:23:14 +0500

    Add Practice6 - builtin_function functions examples

---
+++ practice_6/builtin_function/enumerate_zip_examples.py
@@ -0,0 +1,12 @@
+x = ('apple', 'banana', 'cherry')
+y = enumerate(x)
+
+a = ("John", "Charles", "Mike")
+b = ("Jenny", "Christy", "Monica")
+
+x = zip(a, b)
+
+a = ("John", "Charles", "Mike")
+b = ("Jenny", "Christy", "Monica", "Vicky")
+
+x = zip(a, b)
---
+++ practice_6/builtin_function/map_filter_reduce.py
@@ -0,0 +1,23 @@
+def myfunc(n):
+  return len(n)
+
+x = map(myfunc, ('apple', 'banana', 'cherry'))
+
+def myfunc(a, b):
+  return a + b
+
+x = map(myfunc, ('apple', 'banana', 'cherry'), ('orange', 'lemon', 'pineapple'))
+
+ages = [5, 12, 17, 18, 24, 32]
+
+def myFunc(x):
+  if x < 18:
+    return False
+  else:
+    return True
+
+adults = filter(myFunc, ages)
+
+for x in adults:
+  print(x)
+
